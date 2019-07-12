c25e867635640c29861b6c9c16bcb1df47a504d9
Junior Dugue <jrdugue@gmail.com>
2019-07-11 20:22:23 -0400

    added code needed to test via redux chrome extension

--- redux-ii-master/src/index.js
+++ redux-ii-master/src/index.js
@@ -5,7 +5,10 @@ import { Provider } from 'react-redux'
 import App from './components/App'
 import reducer from './reducer'
 
-const store = createStore(reducer)
+const store = createStore(
+	reducer,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), //code needed to test via redux chrome extension 
+	)
 
 ReactDOM.render(
 	<Provider store={store}>
